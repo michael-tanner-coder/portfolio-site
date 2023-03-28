ed65e497bc37caaa2188901dcdb81f527d59089a
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-03-27 21:34:13 -0500

    Create todo.js

---
+++ todo.js
@@ -0,0 +1,5 @@
+// TODO LIST
+// TODO: make high quality gifs for each game
+// TODO: test contact methods
+// TODO: showcase skillsets / what you bring
+// TODO: write about you (goals, skills, experience)
