56216b116c0be753e720b476229e096f28f94c1a
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-03-30 21:26:48 -0500

    toolset section

--- todo.js
+++ todo.js
@@ -1,13 +1,21 @@
 // BASE
 // TODO: make high quality gifs for each game
-// TODO: create small thumbnails for each game
 // TODO: test contact methods
+
 // TODO: showcase skillsets / what you bring
-// TODO: list your toolset
+// TODO: -- rapid prototyping
+// TODO: -- Gameplay Programming
+// TODO: -- 2D Art
+// TODO: -- UI Design and Development
+// TODO: -- leadership
+// TODO: -- production planning
+
 // TODO: get a new photo of yourself
 // TODO: write about you (goals, skills, experience)
 // TODO: give the page more personality!
 
 // EXTRA
 // TODO: make project pages explaining your contributions
+// TODO: create small thumbnails for each game
+// TODO: make game jam section
 
