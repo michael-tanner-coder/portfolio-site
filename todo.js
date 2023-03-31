8f3205f66bcbc4deb7270e54c51bc240238c70bb
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-03-30 19:44:03 -0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,4 +1,4 @@
-// TODO LIST
+// BASE
 // TODO: make high quality gifs for each game
 // TODO: create small thumbnails for each game
 // TODO: test contact methods
@@ -6,5 +6,8 @@
 // TODO: list your toolset
 // TODO: get a new photo of yourself
 // TODO: write about you (goals, skills, experience)
-// TODO: make project pages explaining your contributions
 // TODO: give the page more personality!
+
+// EXTRA
+// TODO: make project pages explaining your contributions
+
